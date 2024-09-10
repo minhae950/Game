4e578ab041e5c71f0a60ac6e37a082a2c381a1e9
BG Kim <steamkbg0506@naver.com>
2024-09-10 13:20:55 +0900

    add main runtime execution

---
+++ gamert.py
@@ -0,0 +1,4 @@
+import sys
+
+if __name__ == "__main__":
+    print("Arguments Are: " + sys.argv[1] + " / " + sys.argv[2] + " / " + sys.argv[3])
--- main.py
+++ main.py
@@ -2,17 +2,19 @@ import pygame
 import random
 import moviepy.editor as mp
 import sys
+import os
 import time
 import PIL
 PIL.Image.ANTIALIAS = PIL.Image.LANCZOS
 
 def main():
     pygame.init()
-    screen = pygame.display.set_mode((1920, 1080), pygame.FULLSCREEN)
+    screen = pygame.display.set_mode((1366, 768))
     pygame.display.set_caption("벤조디아제핀")
-    intro_vid = mp.VideoFileClip("assets/videos/intro.mp4")
-    # intro_vid = _intro_vid.resize(height=360)
+    _intro_vid = mp.VideoFileClip("assets/videos/intro.mp4")
+    intro_vid = _intro_vid.resize(height=768)
     keepRunning = True
+    execPath = sys.executable + " ./gamert.py"
     clk = pygame.time.Clock() # Sorry for dirty names; clock was already defined in other module
     pixelFont_128 = pygame.font.Font("assets/fonts/neodgm.ttf", 128)
     pixelFont_64 = pygame.font.Font("assets/fonts/neodgm.ttf", 64)
@@ -39,16 +41,16 @@ def main():
     quitText = pixelFont_80.render("종료", False, (0, 0, 0))
     helpText = pixelFont_32.render("WASD 또는 방향키로 조작하고 스페이스바로 선택", True, (0, 0, 0))
     selectText = pixelFont_80.render("►", False, (0, 0, 0))
-    screen.blit(titleText, (530, 200))
-    screen.blit(titleText_kor, (750, 400))
-    screen.blit(playText, (850, 550))
-    screen.blit(quitText, (850, 750))
-    screen.blit(creditText, (850, 650))
+    screen.blit(titleText, (250, 100))
+    screen.blit(titleText_kor, (500, 250))
+    screen.blit(playText, (600, 400))
+    screen.blit(quitText, (600, 600))
+    screen.blit(creditText, (600, 500))
     screen.blit(helpText, (600, 1000))
     pygame.display.update()
 
-    selXPos = 750
-    selYPos = 550
+    selXPos = 500
+    selYPos = 400
     posStatus = 1
 
     while keepRunning:
@@ -58,10 +60,10 @@ def main():
             if event.type == pygame.QUIT:
                 keepRunning = False
             elif event.type == pygame.KEYUP:
-                if (event.key == pygame.K_DOWN or event.key == pygame.K_s) and 540 < selYPos < 660:
+                if (event.key == pygame.K_DOWN or event.key == pygame.K_s) and 390 < selYPos < 510:
                     selYPos = selYPos + 100
                     posStatus += 1
-                if (event.key == pygame.K_UP or event.key == pygame.K_w) and 640 < selYPos < 760:
+                if (event.key == pygame.K_UP or event.key == pygame.K_w) and 490 < selYPos < 610:
                     selYPos = selYPos - 100
                     posStatus -= 1
                 """
@@ -71,21 +73,25 @@ def main():
                 """
                 if event.key == pygame.K_SPACE:
                     if posStatus == 1:
-                        pass # TODO: insert here
+                        # exec(open('./gamert.py').read())
+                        os.system(execPath + " 0 1366 768")
+                        # pygame.quit()
+                        sys.exit()
                     elif posStatus == 2:
                         pass # TODO: insert here
                     elif posStatus == 3:
                         pygame.quit()
                         sys.exit()
             elif event.type == pygame.MOUSEBUTTONDOWN:
-                x = random.randrange(0, 1800)
-                y = random.randrange(0, 900)
-                deg = random.randrange(0, 360)
-                bzdzph_molecule = pygame.transform.rotate(bzdzph_molecule, deg)
-                screen.blit(bzdzph_molecule, (x, y))
+                pass
+                # x = random.randrange(0, 1800)
+                # y = random.randrange(0, 900)
+                # deg = random.randrange(0, 360)
+                # bzdzph_molecule = pygame.transform.rotate(bzdzph_molecule, deg)
+                # screen.blit(bzdzph_molecule, (x, y))
 
 
-            pygame.draw.rect(screen, (255, 255, 255), (750, 550, 95, 350))
+            pygame.draw.rect(screen, (255, 255, 255), (500, 400, 95, 350))
             screen.blit(selectText, (selXPos, selYPos))
             pygame.display.update()
 
